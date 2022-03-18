c28db140fec850655da94dfdab30a4849384a707
Itoi-Mkt <workinfo.mkt@gmail.com>
2022-03-18 10:52:00 +0900

    generate terminal page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'terminals/index'
   get 'user' => 'users#index'
   root 'home#index'
 
---
+++ test/controllers/terminals_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class TerminalsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get terminals_index_url
+    assert_response :success
+  end
+
+end
