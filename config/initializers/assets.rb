8b2bc9b7dc50f749e23a93365a3f3ff2e6277dcc
Itoi-Mkt <workinfo.mkt@gmail.com>
2022-03-22 18:00:52 +0900

    desigin top page & refactor map page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,3 +16,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.precompile += %w[admin.js map.css]
+Rails.application.config.assets.precompile += %w( home.css )
