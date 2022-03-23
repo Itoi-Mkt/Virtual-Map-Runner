d9680b80f18e37d0ab2955c9309bfbc47511a1bc
Itoi-Mkt <workinfo.mkt@gmail.com>
2022-03-23 14:44:19 +0900

    create chizu button

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -4,20 +4,11 @@ class StepsController < ApplicationController
   # skip_before_action :check_logged_in, only: :show
 
   def new
-    # @distance = Distance.new
   end
 
-  
-
   def create
-    pp 'stepsのcreateだよー'
-    pp params
-    # binding.pry
-    
     dis = params.require(:distance)
     dis.each do |element|
-      p "test"
-      
       dis_value = element[0]
       poly_points = element[1]
       @step = Step.new(
@@ -27,27 +18,14 @@ class StepsController < ApplicationController
       )
       # @step.save
     end
-
-    # binding.pry
-
   end
 
   def show
-    # users = { id:1, nickname: "Saiga", age: 22 }
     @steps = Step.all
-    # render json: @steps
-
-
     respond_to do |format|
-      # リクエストされるフォーマットがHTML形式の場合
       format.html 
-
-      # リクエストされるフォーマットがJSON形式の場合
       format.json { render json: @steps } 
-      # @usersをjson形式のデータへ変換して返す
     end
   end
-
-
 
 end
